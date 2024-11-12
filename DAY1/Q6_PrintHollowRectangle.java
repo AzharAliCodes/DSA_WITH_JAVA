47c4879c15a4e2f8a51f068ae7808334dbbd6f9e
AzharAliCodes <azharuddinali017@gmail.com>
2024-11-12 16:35:52 +0530

    add more files of pattern

---
+++ DAY1/Q6_PrintHollowRectangle.java
@@ -0,0 +1,9 @@
+public class Q6_PrintHollowRectangle {  
+    public static void main(String[] args) {
+       for (int i = 0; i < 5; i++) 
+            System.out.print("*");
+    System.out.println();
+       for (int i = 0; i < 5; i++) 
+            System.out.print("*");
+}
+}

57a794bb07dd743e3f1a12280b0caee588f9f175
AzharAliCodes <azharuddinali017@gmail.com>
2025-02-09 21:41:06 +0530

    10

---
+++ DAY8/Q08_AbstractCLass.java
@@ -0,0 +1,10 @@
+/*Proparties of abstract class
+-cannot create an instance of abstract class
+-can have abstract/non-abstract methods
+-can have Constructors
+*/
+package dsa_with_java.DAY8;
+
+public class Q08_AbstractCLass {
+    
+}
